6bab845d305b8a86b67c862690cac5fb2e87eea9
danggiap <quydo0609@gmail.com>
2026-01-12 22:27:30 +0700

    update cross fix 1

--- src/main/java/org/example/apiquanlythuvien/config/WebConfig.java
+++ src/main/java/org/example/apiquanlythuvien/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     WebMvcConfigurer.super.addCorsMappings(registry);
     registry.addMapping("/**")
-        .allowedOriginPatterns("*")
+        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5173")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
--- src/main/java/org/example/apiquanlythuvien/security/SecurityConfig.java
+++ src/main/java/org/example/apiquanlythuvien/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.example.apiquanlythuvien.service.account.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,6 +36,7 @@ public class SecurityConfig {
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.
         csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/api/accounts/login", "/api/sach/all").permitAll()
