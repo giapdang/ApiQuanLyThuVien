22678cd05f5df982dab57829eb4abd66210db8ba
danggiap <quydo0609@gmail.com>
2026-01-19 12:58:01 +0700

    config fix cross

--- src/main/java/org/example/apiquanlythuvien/config/WebConfig.java
+++ src/main/java/org/example/apiquanlythuvien/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     WebMvcConfigurer.super.addCorsMappings(registry);
     registry.addMapping("/**")
-        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5173")
+        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:5173", "http://localhost:8080")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
